c9ad05032cdc03806aabb3e32a29cde75b383dca
101da <davidmueller85@gmail.com>
2016-05-29 18:01:12 +0200

    Styles / Efficiency / Statement Accuracy
    Code fails at line 17.

--- Script.js
+++ Script.js
@@ -1,55 +1,31 @@
 var d = new Date();
-var x = d.getDay();
-debugger;
-var Weekdays = ["Sonntag", "Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag"];
-debugger;
+var day = d.getDay();
+var hour = d.getHours();
 
-var y = d.getHours();
-debugger;
-switch (y) {
-  case 0:
-  case 1:
-  case 2:
-  case 3:
-  case 4:
-  case 5:
-  case 6:
-  case 7:
-  case 8:
-  case 9:
-  case 10:
-  case 11:
-      hour = "Der Tag ist noch jung, wir w@uml;nschen einen sch@oml;nen " + Weekdays[x] + ".";
-      break;
-  case 12:
-  case 13:
-  case 14:
-  case 15:
-      hour = "Guten Hunger und noch einen schönen " + Weekdays[x] + ".";
-      break;
-  case 16:
-  case 17:
-  case 18:
-  case 19:
-      hour = "Wir wünschen Ihnen einen entspannten Feierabend und noch einen schönen " +
-      Weekdays[x] + ".";
-      break;
-  case 20:
-  case 21:
-  case 22:
-  case 23:
-      hour = "Noch einen schönen Restabend wünschen wir Ihnen am " + Weekdays[x] + ".";
-      break;
-  default:
-      hour = "Wir wünschen Ihnen einen schönen Tag.";
+var Weekdays = ["Sonntag", "Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag"];
+var greeting = "";
+
+if (hour <= 11) {
+  greeting = "Der Tag ist noch jung, wir wünschen einen schönen " + Weekdays[day] + ".";
+} else if (hour <= 15 ) {
+  greeting = "Guten Hunger und noch einen schönen " + Weekdays[day] + ".";
+} else if (hour <= 19) {
+  greeting = "Wir wünschen Ihnen einen entspannten Feierabend und noch einen schönen " +
+  Weekdays[day] + ".";
+} else if (hour <= 23) {
+  greeting = "Noch einen schönen Restabend wünschen wir Ihnen am " + Weekdays[day] + ".";
+} else if ((day = 0 || day = 6) && (hour <= 19)) {
+  greeting = "Wir wünschen Ihnen ein entspanntes Wochenende und noch einen schönen " +
+  Weekdays[day] + ".";
+} else {
+  greeting = "Wir wünschen Ihnen einen schönen Tag.";
 }
 
-//document.getElementById("demo").innerHTML = Weekdays[x];
-document.getElementById("demo").innerHTML = hour;
-
-
-
-
+//document.getElementById("demo").innerHTML = Weekdays[day];
+var changeHtml = document.getElementById("demo");
+changeHtml.innerHTML = greeting;
+changeHtml.style.color = "red";
+changeHtml.style.fontFamily = "Arial";
 
 
 //document.getElementById("demo").innerHTML = d.getDay();
