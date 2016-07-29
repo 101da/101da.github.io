9878baa60b4beae3451826c80c0ce679152298ee
101da <davidmueller85@gmail.com>
2016-07-29 21:46:13 +0200

    Changes where made
    Started coding again..

---
+++ testSvg/testSvg.js
@@ -0,0 +1,7 @@
+var s = Snap();
+// Circle with 80px radius
+var circle = s.circle(100, 100, 50);
+// Square with 160px side width
+var square = s.rect(210,40,160,160);
+// Ellipse with 80px vertical radius and 50px horizontal radius
+var ellipse = s.ellipse(460,120,50,80);
