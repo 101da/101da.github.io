87b13bb37f86087aecf92db8487b07d79e47d5e8
101da <davidmueller85@gmail.com>
2016-09-04 16:01:26 +0200

    sidebar-menu
    The siedebar-menu is mostly finished.
    Problems:
    -no scrolling possible
    -sidebar isn't responsive jet

--- Start.js
+++ Start.js
@@ -1,6 +1,16 @@
+function visible() {document.getElementById('list-elements').style.display = 'inline'};
+
 function openNav() {
     document.getElementById("list").style.width = "15em";
+    setTimeout(visible, 150);
 }
 function closeNav() {
     document.getElementById("list").style.width = "0em";
+    document.getElementById('list-elements').style.display ='none';
 }
+var windowHeight = window.innerHeight;
+var windowWidth = window.innerWidth;
+console.log("Displaybreite: " + windowWidth);
+console.log("Displayhoehe: " + windowHeight);
+document.getElementById('headerImage').style.height = windowHeight + "px";
+document.getElementById('article').style.marginTop = windowHeight + "px";
