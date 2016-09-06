d2c0306eec43216a212f962c9ba13b394b6734a5
101da <davidmueller85@gmail.com>
2016-09-06 21:27:16 +0200

    Night vision
    Changed the color at night to black/white with js (timed at 20:00)

--- Start.js
+++ Start.js
@@ -1,3 +1,4 @@
+//navbar
 function visible() {document.getElementById('list-elements').style.display = 'inline'};
 
 function openNav() {
@@ -8,9 +9,26 @@ function closeNav() {
     document.getElementById("list").style.width = "0em";
     document.getElementById('list-elements').style.display ='none';
 }
+
+
+//optional displaySize
 var windowHeight = window.innerHeight;
 var windowWidth = window.innerWidth;
 console.log("Displaybreite: " + windowWidth);
 console.log("Displayhoehe: " + windowHeight);
 //>>document.getElementById('headerImage').style.height = windowHeight + "px";
 //document.getElementById('article').style.marginTop = windowHeight + "px";
+
+
+//dayNnight
+var d = new Date();
+var hour =d.getHours();
+var bodyId = document.getElementById('body');
+var aId = document.getElementsByName('a');
+
+//the a-element wont turn white yet.
+if (hour => 20) {
+  bodyId.style.color = "white";
+  bodyId.style.backgroundColor = "black";
+  aId.style.color = "white";
+} else {}
